2313b81a42fba512365068ee7b147c066c376149
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2018-07-09 17:05:57 +0530

    Partly functional resolvers.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # wallpapers-server
+
 Back-end for wallpapers. Former submodule of wallpapers repo.
+
+Uses Google Cloud Datastore and Cloud Storage, but supports MongoDB and a local folder for local development.
+
+Note: MongoDB and local folder development is NOT SUPPORTED at the moment. This is to get with initial setup as quick as possible.
+
+[For authentication with Google Cloud, see here.](https://cloud.google.com/docs/authentication/getting-started)
+
+`settings.json5`
+
+```json
+{
+  "database": "<datastore or mongodb>",
+  "storage": "<folder or cloud-storage>",
+  // Required for MongoDB.
+  "mongodburl": "<url to MongoDB instance>",
+  // Required for Google Cloud to authenticate if environment variable not provided
+  "authjsonpath": "<path to JSON>",
+  // Required for Google Cloud every time
+  "projectId": "<ID of your project>",
+  // Required for folder
+  "folderPath": "<path to folder with wallpapers>",
+  // Required for Cloud Storage
+  "bucketName": "<name of bucket w/ wallpapers>"
+}
+```
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,7 +1,48 @@
+// Import the Google Cloud APIs.
+import * as Datastore from '@google-cloud/datastore'
+import * as Storage from '@google-cloud/storage'
+// Import the settings we need to use.
+import 'json5/lib/register'
+import { // eslint-disable-next-line no-unused-vars
+  database, storage, mongourl, authjsonpath, projectId, folderPath, bucketName
+} from '../settings.json5'
+
+// Initialize connection to Datastore and Cloud Storage.
+// Potentially: MongoDB, Spanner, Firestore?
+const keyFilename = process.env.GOOGLE_APPLICATION_CREDENTIALS || authjsonpath
+const datastore = new Datastore({ projectId, keyFilename })
+const cloudStorage = Storage({ projectId, keyFilename })
+// Get the bucket and verify its existence.
+const bucket = cloudStorage.bucket(bucketName)
+bucket.exists().then(i => i[0] || console.error('This bucket does not exist!')).catch(console.error)
+
+// Export our resolvers.
 export default {
   Query: {
-    getUpvotes: (_, { imageId }) => 10,
-    upvoteAndQuery: (_, { imageId, deviceId }) => 10,
-    downvoteAndQuery: (_, { imageId, deviceId }) => 10
+    // Get upvotes through this query.
+    getUpvotes: (_, { imageId }) => datastore.get(datastore.key(['Image', imageId.toString()]))
+      .then(i => i[0]['upvotes'].length), // If entity didn't exist, upvotes = 0.
+    // Upvote through this query.
+    upvoteAndQuery: async (_, { imageId, deviceId }) => {
+      // Ensure the entity exists before upvoting.
+      const key = datastore.key(['Image', imageId.toString()])
+      await datastore.save({ key, data: {} })
+      const data = (await datastore.get(key))[0]
+      if (data['upvotes']) data['upvotes'].push(deviceId)
+      else data['upvotes'] = [deviceId]
+      datastore.save({ key, data })
+      return data['upvotes'].length
+    },
+    downvoteAndQuery: async (_, { imageId, deviceId }) => {
+      // Ensure the entity exists before upvoting.
+      const key = datastore.key(['Image', imageId.toString()])
+      await datastore.save({ key, data: {} })
+      const data = (await datastore.get(key))[0]
+      if (data['upvotes'].includes(deviceId)) {
+        data['upvotes'].splice(data['upvotes'].indexOf(deviceId), 1)
+      }
+      datastore.save({ key, data })
+      return data['upvotes'].length
+    }
   }
 }
