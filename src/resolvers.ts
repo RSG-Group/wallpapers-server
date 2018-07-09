6ebffe1c3da4b6e368316972b104364112e07161
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2018-07-09 19:35:01 +0530

    Finish Datastore communication.

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -4,11 +4,11 @@ import * as Storage from '@google-cloud/storage'
 // Import the settings we need to use.
 import 'json5/lib/register'
 import { // eslint-disable-next-line no-unused-vars
-  database, storage, mongourl, authjsonpath, projectId, folderPath, bucketName
+  database, storage, mongourl, folderPath, authjsonpath, projectId, bucketName
 } from '../settings.json5'
 
 // Initialize connection to Datastore and Cloud Storage.
-// Potentially: MongoDB, Spanner, Firestore?
+// Potentially: MongoDB or Firestore?
 const keyFilename = process.env.GOOGLE_APPLICATION_CREDENTIALS || authjsonpath
 const datastore = new Datastore({ projectId, keyFilename })
 const cloudStorage = Storage({ projectId, keyFilename })
@@ -20,25 +20,40 @@ bucket.exists().then(i => i[0] || console.error('This bucket does not exist!')).
 export default {
   Query: {
     // Get upvotes through this query.
-    getUpvotes: (_, { imageId }) => datastore.get(datastore.key(['Image', imageId.toString()]))
-      .then(i => i[0]['upvotes'].length), // If entity didn't exist, upvotes = 0.
-    // Upvote through this query.
-    upvoteAndQuery: async (_, { imageId, deviceId }) => {
+    getUpvotes: async (_, { imageId }) => {
       // Ensure the entity exists before upvoting.
       const key = datastore.key(['Image', imageId.toString()])
-      await datastore.save({ key, data: {} })
-      const data = (await datastore.get(key))[0]
-      if (data['upvotes']) data['upvotes'].push(deviceId)
-      else data['upvotes'] = [deviceId]
+      let data = (await datastore.get(key))[0]
+      if (!data) {
+        data = await datastore.insert({ key, data: { upvotes: [] } })
+        data = (await datastore.get(key))[0]
+      }
+      datastore.save({ key, data })
+      return data['upvotes'].length
+    }
+  },
+  Mutation: {
+    // Upvote through this query.
+    upvote: async (_, { imageId, deviceId }) => {
+      // Ensure the entity exists before upvoting.
+      const key = datastore.key(['Image', imageId.toString()])
+      let data = (await datastore.get(key))[0]
+      if (!data) {
+        data = await datastore.insert({ key, data: { upvotes: [deviceId] } })
+        data = (await datastore.get(key))[0]
+      } else if (!data['upvotes'].includes(deviceId)) data['upvotes'].push(deviceId)
       datastore.save({ key, data })
       return data['upvotes'].length
     },
-    downvoteAndQuery: async (_, { imageId, deviceId }) => {
+    // Remove upvote through this query.
+    downvoteIfVoted: async (_, { imageId, deviceId }) => {
       // Ensure the entity exists before upvoting.
       const key = datastore.key(['Image', imageId.toString()])
-      await datastore.save({ key, data: {} })
-      const data = (await datastore.get(key))[0]
-      if (data['upvotes'].includes(deviceId)) {
+      let data = (await datastore.get(key))[0]
+      if (!data) {
+        data = await datastore.insert({ key, data: { upvotes: [] } })
+        data = (await datastore.get(key))[0]
+      } else if (data['upvotes'].includes(deviceId)) {
         data['upvotes'].splice(data['upvotes'].indexOf(deviceId), 1)
       }
       datastore.save({ key, data })
